8d20b18cf4d58aae3b96e06d165f423655b7aee4
John Agapeyev <jagapeyev@gmail.com>
2018-01-25 16:52:41 -0800

    Added cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+#Drop tcp existing rules
+iptables -F
+
+#Drop tcp existing non-default chains
+iptables -X
+
